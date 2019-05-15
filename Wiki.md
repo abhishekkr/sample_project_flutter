ebb84cf60dafdba3fe42b6b7288f2f2fbabc2c36
AbhishekKr <abhikumar163@gmail.com>
2019-05-15 17:07:41 +0530

    stage 2 code and wiki for Codelabs First Flutter App
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

---
+++ Wiki.md
@@ -0,0 +1,23 @@
+
+## Codelabs: First Flutter App
+
+[source](https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/#1)
+
+### Creating an inital flutter app
+
+* core of app code resides at `./lib/main.dart`
+
+> * creates a material app
+>
+> * main method uses arrow (=>) notation, used for one-line functions
+>
+> * app extends StatelessWidget, making app itself a widget
+>
+> * in flutter, almost everything is a widget, including alignment, padding, and layout
+>
+> * Scaffold widget, provides a default app bar, a title, and a body property that holds the widget tree for the home screen
+
+
+* `flutter format <filename>`
+
+---
--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,19 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';     // creates a material app
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp());             // main method uses arrow notation; for one-liner function
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {       // app extgends StatelessWidget, making the app itself a widget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fluttr',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Fluttr-ing'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fluttr-ing'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        body: const Center(
+          child: const Text('Hey Flocks'),
+        ),
       ),
     );
   }
